d37fcd20453e0ffaca45304b8500eca2875e77c3
gael-src <gaelfr85@gmail.com>
2020-05-24 13:55:40 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -3,20 +3,23 @@ import React from "react";
 import "./App.css";
 
 function App() {
+  const state = {
+    clients: [
+      { id: 1, name: "Item 1" },
+      { id: 2, name: "Item 2" },
+      { id: 3, name: "Item 3" },
+    ],
+  };
+  const listElements = state.clients.map((client) => {
+  return <li>{client.name} <button>x</button></li>
+  })
+
   return (
     <div className="main-container">
       <h1>Chamla</h1>
       <h2>Liste</h2>
       <ul>
-        <li>
-          item 1 <button>x</button>
-        </li>
-        <li>
-          item 2 <button>x</button>
-        </li>
-        <li>
-          item 3 <button>x</button>
-        </li>
+        {listElements}
       </ul>
       <form>
         <input type="text" placeholder="Add List Item"></input>
