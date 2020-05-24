1ed789c9019680e1a1324344f0b438f620ea841f
gael-src <gaelfr85@gmail.com>
2020-05-24 13:37:19 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <h1>Chamla</h1>
+    <div className="main-container">
+      <h1>Chamla</h1>
+      <h2>Liste</h2>
+      <ul>
+        <li>
+          item 1 <button>x</button>
+        </li>
+        <li>
+          item 2 <button>x</button>
+        </li>
+        <li>
+          item 3 <button>x</button>
+        </li>
+      </ul>
+      <form>
+        <input type="text" placeholder="Add List Item"></input>
+        <button>Confirm</button>
+      </form>
+    </div>
   );
 }
 
